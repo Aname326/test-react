7265244fbae667f2db166fc0f88fbe403c42c9c4
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-07-26 12:00:24 +1200

    getStorage function configuratiom

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,4 +25,5 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app)
+export const db = getFirestore(app);
+export const storage = getStorage(app);
