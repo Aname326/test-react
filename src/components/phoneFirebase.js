2d7927f6c7f7a7c5d007c8c1aaa18996cf8169c3
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2024-08-04 20:40:20 +1200

    phone auth firebase config

---
+++ src/components/phoneFirebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAdOUklmU4WLyJvtBc0jE0D3m7mzvMeU7I",
+  authDomain: "react-phone-number-30311.firebaseapp.com",
+  projectId: "react-phone-number-30311",
+  storageBucket: "react-phone-number-30311.appspot.com",
+  messagingSenderId: "302515990473",
+  appId: "1:302515990473:web:d3c54151e5f8313f370218",
+  measurementId: "G-MFCR56NM45"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
